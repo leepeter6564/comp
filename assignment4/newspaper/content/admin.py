c6a1f36b7ab4e5e2262cf41a316b378fdea06028
Peter Lee <seungminlee@college.harvard.edu>
2016-03-19 01:37:05 -0400

    customized admin page

--- assignment4/newspaper/content/admin.py
+++ assignment4/newspaper/content/admin.py
@@ -2,8 +2,26 @@ from django.contrib import admin
 
 from .models import Article, Image, Contributor
 
-admin.site.register(Article)
-admin.site.register(Image)
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+    ('Title/Subtitle Information', {'fields': ['title', 'subtitle']}),
+    ('Publication Date', {'fields': ['pub_date']}),
+    ('Writer', {'fields': ['contributors']}),
+    ('Content', {'fields': ['text']}),
+    ]
+    list_display = ('title', 'pub_date')
+
+class ImageAdmin(admin.ModelAdmin):
+    fieldsets = [
+    ('Title/Subtitle Information', {'fields': ['title', 'subtitle']}),
+    ('Publication Date', {'fields': ['pub_date']}),
+    ('Photographer/Image Editor', {'fields': ['contributors']}),
+    ('Image Location', {'fields': ['path']}),
+    ]
+    list_display = ('title', 'pub_date')
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Contributor)
 
 # username: admin
